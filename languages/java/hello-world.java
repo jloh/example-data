7d9c3c963f4141221c3a931c5ed369888687ba8e
jloh <github@jloh.co>
2017-12-15 23:57:32 +1100

    [Java] add in hello-world

---
+++ languages/java/hello-world.java
@@ -0,0 +1,10 @@
+// Simple hello world example in java
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+    }
+
+}
